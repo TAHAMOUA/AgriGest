82b486885bd62bbf31a87483a44f43ba56c8e2c1
TAHAMOUA <mouaddinetaha@gmail.com>
2026-07-10 14:54:23 +0100

    feat: display validation error messages

--- resources/views/parcelles/create.blade.php
+++ resources/views/parcelles/create.blade.php
@@ -12,7 +12,19 @@
 <div class="container mt-5">
 
     <h2 class="mb-4">Ajouter une Parcelle</h2>
+    @if ($errors->any())
+        <div class="alert alert-danger">
 
+            <ul class="mb-0">
+
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+
+            </ul>
+
+        </div>
+    @endif
     <form action="{{ route('parcelles.store') }}" method="POST">
 
         @csrf
